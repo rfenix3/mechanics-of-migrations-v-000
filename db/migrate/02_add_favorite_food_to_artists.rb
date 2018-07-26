f25cfd6e388f11ec98abcc6e7197070a160264e5
Richard Fenix <richard.fenix@gmail.com>
2018-07-26 16:13:49 +0000

    Done.

---
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -0,0 +1,11 @@
+#class AddFavoriteFoodToArtists < ActiveRecord::Migration
+  # def change
+  #   add_column :artists,:favorite_food, :string 
+  # end
+#end
+class AddFavoriteFoodToArtists < ActiveRecord::Migration
+  def change
+    add_column :artists, :favorite_food, :string
+  end
+end
+

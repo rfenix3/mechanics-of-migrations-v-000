d5da5ec235fbef64f082591c4b2234c28fc8ccaf
Richard Fenix <richard.fenix@gmail.com>
2018-07-27 05:52:00 +0000

    automatically backed up by learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #class AddFavoriteFoodToArtists < ActiveRecord::Migration
   # def change
   #   add_column :artists,:favorite_food, :string 
@@ -9,3 +10,12 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration
   end
 end
 
+=======
+class AddFavoriteFoodToArtists < ActiveRecord::Migration
+  def change
+    add_column
+    :artists,
+    :favorite_food, :string 
+  end
+end
+>>>>>>> 46bea3efc4fabc5e6105c5c4476f164d95fcf712
